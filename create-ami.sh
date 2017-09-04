ab01c261e086d2d07648f7733200ce503ec02390
kolargol <zbyszek@onefellow.com>
2017-09-04 13:54:58 +0200

    Changed default TZ to CET

--- create-ami.sh
+++ create-ami.sh
@@ -148,7 +148,7 @@ create_img() {
 	echo "127.0.0.1\tlocalhost" >${_MNT}/etc/hosts
 	echo "::1\t\tlocalhost" >>${_MNT}/etc/hosts
 	sed -i "s/^#\(PermitRootLogin\) .*/\1 no/" ${_MNT}/etc/ssh/sshd_config
-	chroot ${_MNT} ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+	chroot ${_MNT} ln -sf /usr/share/zoneinfo/CET /etc/localtime
 	chroot ${_MNT} ldconfig /usr/local/lib /usr/X11R6/lib
 	chroot ${_MNT} rcctl disable sndiod
 	chroot ${_MNT} useradd -G wheel -L staff -c 'EC2 Default User' -g =uid \
