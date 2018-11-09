c2b44899d6223b551ef2e95ee7bcfeec56476f3c
Zbyszek Żółkiewski <zbyszek@onefellow.com>
2018-11-09 13:49:49 +0100

    Use official CD for OpenBSD

--- create-ami.sh
+++ create-ami.sh
@@ -128,7 +128,7 @@ create_img() {
 
 	pr_action "configuring the image"
 	# XXX hardcoded
-	echo "http://mirror.switch.ch/ftp/pub/OpenBSD" >${_MNT}/etc/installurl
+	echo "https://cdn.openbsd.org/pub/OpenBSD" >${_MNT}/etc/installurl
 	sed -e "s#\(/home ffs rw\)#\1,nodev,nosuid#" \
 		-e "s#\(/tmp ffs rw\)#\1,nodev,nosuid#" \
 		-e "s#\(/usr ffs rw\)#\1,nodev#" \
